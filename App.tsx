818ae6d3d67681c710ce228e918a28053406e50d
DJ3125 <dylanalexja@gmail.com>
2024-11-15 07:19:59 -0600

    initial commit2

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {useState, useRef, useEffect} from 'react';
+
+import {Gyroscope} from 'expo-sensors';
+
 export default function App() {
+  const gyroData = useRef({x: 0, y: 0, z: 0,});
+  const [data, setData] = useState(gyroData);
+  useEffect(function(){
+    
+  }, [gyroData.current.x]);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
